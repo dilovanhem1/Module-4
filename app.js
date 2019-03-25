897fd4835a08ddca46856ec032eebb2e51d827c7
dilovanhem1 <48559385+dilovanhem1@users.noreply.github.com>
2019-03-25 13:42:04 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+YaakovGreeter.sayHello ();
+JohnGreeter.sayHello ();
+JoeGreeter.sayHello ();
+MarieGreeter.sayHello ();
+
+
+// Immediately Invoked Function Expression
+// IIFE
+//(function (name) {
+//	console.log("Hello " + name);
+//})("Coursera!");
+  
